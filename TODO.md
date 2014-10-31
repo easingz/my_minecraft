0fe150bd0c35ae558bbf76f7cd8243bdbe9b9325
Yixing Zhang <yixingz@opera.com>
2014-10-31 11:11:33 +0800

    plan next step work.

--- TODO.md
+++ TODO.md
@@ -2,3 +2,6 @@
 - [ ] hidden faces removal.
 - [ ] implement collision detection.
 - [ ] implement log module.
+- [ ] cut down triangles when generates terrain(adjacent face removal, down-side removal)
+- [ ] merge draw calls by merge triangle&texture data in one buffer as much as possible.
+- [ ] add comments to APIs of every module
---
+++ terrain.h
@@ -0,0 +1,9 @@
+/**
+ * Dedicate to generate terrain, which merges all unit and remove unneccesary faces like down-side
+ * and adjacent ones.
+ * TODO:
+ *   o Consider flat terrain only first
+ *   o two pass generation: one for position generation, one for face merge and cut.
+ *   o make terrain data load&store-able.
+ *   o consider complex terrain generation.
+ */
